980471cb32641971cd7f39cf0413228c46a637ef
Fuad Talukder <webfuadtk@gmail.com>
2024-02-15 12:32:35 +0600

    complete header & footer

--- src/pages/footer/Footer.tsx
+++ src/pages/footer/Footer.tsx
@@ -1,10 +1,77 @@
 import Container from "../../components/container/Container";
+import { FaEnvelope } from "react-icons/fa";
+import { IoIosCall } from "react-icons/io";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { SlSocialPintarest } from "react-icons/sl";
 
 const Footer = () => {
   return (
-    <Container>
-      <h1>This is footer</h1>
-    </Container>
+    <div className="bg-[#1E2436] text-white">
+      <Container>
+        <footer className="footer grid grid-cols-12 gap-10 text-white py-16">
+          <aside className="col-span-4">
+            <p>
+              <span className="text-3xl font-bold">
+                Food<span className="text-[#e7c14d]">Sphere</span>
+              </span>
+            </p>
+            <p className="max-w-[40ch] mt-3 text-lg">
+              Nulla ultricies justo sit amet ante efficitur, eget pharetra augue
+              efficitur. Vestibulum viverra, dolor sit amet ultricies.
+            </p>
+            <button className="border-2 border-[#e7c14d] px-6 py-2 rounded-lg mt-6 font-bold">
+              Donate Now
+            </button>
+          </aside>
+          <nav className="col-span-2">
+            <h6 className="font-bold text-2xl text-[#e7c14d]">Services</h6>
+            <a className="link link-hover text-lg">Donate</a>
+            <a className="link link-hover text-lg">Sponsor</a>
+            <a className="link link-hover text-lg">Fundraise</a>
+            <a className="link link-hover text-lg">Volunteer</a>
+            <a className="link link-hover text-lg">Partner</a>
+          </nav>
+          <nav className="col-span-3">
+            <h6 className="font-bold text-2xl text-[#e7c14d]">Non profit</h6>
+            <a className="link link-hover text-lg">Differently Abled Kids</a>
+            <a className="link link-hover text-lg">Help Child Cancer</a>
+            <a className="link link-hover text-lg">Clean Pure Water</a>
+            <a className="link link-hover text-lg">Give them Education</a>
+            <a className="link link-hover text-lg">Start a Fundraising</a>
+          </nav>
+          <nav className="col-span-4">
+            <h6 className="font-bold text-2xl text-[#e7c14d]">Contact</h6>
+            <p className="text-lg">
+              380 Street Kilda Broklyn Road Melbourne Australia
+            </p>
+            <p className="flex gap-3 justify-center items-center text-lg">
+              <FaEnvelope className="text-[#e7c14d] text-xl"></FaEnvelope>
+              foodSphere@gmail.com
+            </p>
+            <p className="flex gap-3 justify-center items-center text-lg">
+              <IoIosCall className="text-[#e7c14d] text-2xl"></IoIosCall>+ 98
+              (000) - 9630
+            </p>
+            <p className="mt-1 flex gap-3">
+              <button className="bg-[#00715D] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaFacebook className="text-[#e7c14d] text-xl"></FaFacebook>
+              </button>
+              <button className="bg-[#00715D] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaInstagram className="text-[#e7c14d] text-xl"></FaInstagram>
+              </button>
+              <button className="bg-[#00715D] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <FaTwitter className="text-[#e7c14d] text-xl"></FaTwitter>
+              </button>
+              <button className="bg-[#00715D] w-[40px] h-[40px] flex items-center justify-center rounded-full">
+                <SlSocialPintarest className="text-[#e7c14d] text-xl"></SlSocialPintarest>
+              </button>
+            </p>
+          </nav>
+        </footer>
+      </Container>
+    </div>
   );
 };
 
--- src/pages/navBar/NavBar.tsx
+++ src/pages/navBar/NavBar.tsx
@@ -1,9 +1,71 @@
 import Container from "../../components/container/Container";
+import { Link } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
 
 const NavBar = () => {
   return (
     <Container>
-      <h1>This is NavBar</h1>
+      <div className="navbar pt-4">
+        <div className="navbar-start flex justify-between">
+          <p>
+            <span className="text-3xl font-bold text-[#00715D]">
+              Food<span className="text-[#e7c14d]">Sphere</span>
+            </span>
+          </p>
+        </div>
+        {/*  #62cdee*/}
+        <div className="navbar-end hidden lg:flex">
+          <ul className="md:flex items-center space-x-4 font-semibold p-5">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="all-supplies">All Supplies</Link>
+            </li>
+            <li>
+              <Link to="dashboardLayout">Dashboard</Link>
+            </li>
+            <li>
+              <button className="border-2 border-[#e7c14d] px-6 py-2 rounded-lg">
+                <Link to="dashboardLayout">Login</Link>
+              </button>
+            </li>
+          </ul>
+        </div>
+        {/*  */}
+        <div className="dropdown navbar-end md:hidden flex">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn text-5xl btn-ghost lg:hidden text-[#0B63E5]"
+          >
+            <IoMenu></IoMenu>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-48 z-[1] p-2 shadow bg-slate-200 w-36"
+          >
+            <li>
+              <Link to="/" className="bg-[#0B63E5] text-white">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="all-supplies" className="bg-[#0B63E5] text-white mt-1">
+                All Supplies
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="dashboardLayout"
+                className="bg-[#0B63E5] text-white mt-1"
+              >
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </Container>
   );
 };
