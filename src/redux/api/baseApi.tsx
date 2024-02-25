b951dc812d098675bab450d47b14bbd6e4c6fd2a
Fuad Talukder <webfuadtk@gmail.com>
2024-02-25 15:25:10 +0600

    set servel url

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/",
+    baseUrl: "https://l2-b2-assignment-6-backend-fuad-talukder85.vercel.app",
     credentials: "include",
   }),
   endpoints: () => ({}),
